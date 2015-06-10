b81f0bace6358a718d4541d5d0d33870b400ec44
Elliot <elliot.schrock@gmail.com>
2015-06-10 19:33:28 -0400

    Initial Commit

---
+++ GTFS StationsTests/GTFS_StationsTests.swift
@@ -0,0 +1,36 @@
+//
+//  GTFS_StationsTests.swift
+//  GTFS StationsTests
+//
+//  Created by Elliot Schrock on 6/10/15.
+//  Copyright (c) 2015 Elliot Schrock. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GTFS_StationsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
