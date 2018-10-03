6bf9c7534d8c180eaee9d3ca87d0a7c0307463ac
Elliot <elliot.schrock@gmail.com>
2018-10-03 14:26:57 -0700

    add more functions

--- Pod/Classes/NYCStationManager.swift
+++ Pod/Classes/NYCStationManager.swift
@@ -148,6 +148,88 @@ open class NYCStationManager: NSObject, StationManager {
         return routeIds
     }
     
+    open func stopsNearby(station: Station) -> [String] {
+        var stopIds = [String]()
+        let childStopIds = self.childStopIds(for: station)
+        let stopTimes = Table("stop_times")
+        let sequenceExp = Expression<Int64>("stop_sequence")
+        let stopIdExp = Expression<String>("stop_id")
+        let tripIdExp = Expression<String>("trip_id")
+        let filteredStopTimes = stopTimes.select(sequenceExp, tripIdExp).filter(childStopIds.contains(stopIdExp)).group(tripIdExp)
+        var tripStopIds = [String]()
+        do {
+            for stopTime in try dbManager.database.prepare(filteredStopTimes) {
+                let sequence = stopTime[sequenceExp]
+                var sequences = [Int64]()
+                if sequence > 2 { sequences.append(sequence - 2) }
+                if sequence > 1 { sequences.append(sequence - 1) }
+                sequences.append(sequence + 1)
+                sequences.append(sequence + 2)
+                let nearbyIdsStatement = stopTimes.filter(tripIdExp == stopTime[tripIdExp] && sequences.contains(sequenceExp))
+                let nearbyIdResults = try dbManager.database.prepare(nearbyIdsStatement)
+                let nearbyIds = nearbyIdResults.map({ String($0[stopIdExp].prefix($0[stopIdExp].count - 1)) })
+                tripStopIds.append(contentsOf: nearbyIds)
+            }
+            for stopId in Array(Set(tripStopIds)) {
+                stopIds.append(contentsOf: transferIds(for: stopId))
+            }
+        } catch {
+            print(error)
+        }
+        return Array(Set(stopIds))
+    }
+    
+    open func numberOfStopsBetween(_ station: Station, _ stopId: String, _ routeId: String, _ directionId: Int64) -> Int64 {
+        let otherStation = stationForParentStop(stop: NYCStop(name: "", objectId: stopId, parentId: nil))
+        let stopTimes = Table("stop_times")
+        let trips = Table("trips")
+        let sequenceExp = Expression<Int64>("stop_sequence")
+        let directionIdExp = Expression<Int64>("direction_id")
+        let tripIdExp = Expression<String>("trip_id")
+        let stopIdExp = Expression<String>("stop_id")
+        let routeIdExp = Expression<String>("route_id")
+        
+        let stationChildren = childStopIds(for: station)
+        let otherChildren = childStopIds(for: otherStation)
+        
+        let timesJoinTrips = stopTimes.join(trips, on: stopTimes[tripIdExp] == trips[tripIdExp])
+        let otherStopTimesStatement = timesJoinTrips.filter(routeId == routeIdExp && directionId == directionIdExp && otherChildren.contains(stopIdExp)).limit(1)
+        let stationStopTimesStatement = timesJoinTrips.filter(routeId == routeIdExp && directionId == directionIdExp && stationChildren.contains(stopIdExp)).limit(1)
+        do {
+            let otherStopTimes = try dbManager.database.prepare(otherStopTimesStatement)
+            let stationStopTimes = try dbManager.database.prepare(stationStopTimesStatement)
+            for stationTime in stationStopTimes {
+                for otherTime in otherStopTimes {
+                    return stationTime[sequenceExp] as Int64 - otherTime[sequenceExp] as Int64
+                }
+            }
+        } catch {
+            print(error)
+        }
+        return -1
+    }
+    
+    func childStopIds(for station: Station) -> [String] {
+        var ids = [String]()
+        for stop in station.stops {
+            ids.append("\(stop.objectId!)N")
+            ids.append("\(stop.objectId!)S")
+        }
+        return ids
+    }
+    
+    func transferIds(for stopId: String) -> [String]  {
+        let transfers = Table("transfer")
+        let fromId = Expression<String>("from_id")
+        let toId = Expression<String>("to_id")
+        let statement = transfers.filter(fromId == stopId)
+        do {
+            return try dbManager.database.prepare(statement).map({ $0[toId] })
+        } catch {
+            return [String]()
+        }
+    }
+    
     func stationForParentStop(stop: Stop) -> Station {
         let station = NYCStation(name: stop.name)
         station.primaryId = stop.objectId
